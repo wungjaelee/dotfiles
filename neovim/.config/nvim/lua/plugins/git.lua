4aaf92bece81f3ff2a05c2415b281e1d4023abe0
wungjae <wungjae@uber.com>
2026-07-24 22:05:05 +0000

    Add diffview.nvim with a leader shortcut to diff against tracking branch

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -9,4 +9,24 @@ return {
     event = 'BufWinEnter',
     opts = { current_line_blame = true },  -- who last touched this line
   },
+  {
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewClose' },
+    keys = {
+      {
+        '<leader>gv',
+        function()
+          -- A...B rev-ranges pin both sides to read-only git blobs, so diff against
+          -- the merge-base as a single rev instead - that keeps the working tree side editable.
+          local merge_base = vim.fn.systemlist({ 'git', 'merge-base', '@{upstream}', 'HEAD' })[1]
+          if vim.v.shell_error ~= 0 or not merge_base then
+            vim.notify('No tracking branch configured for this branch', vim.log.levels.ERROR)
+            return
+          end
+          vim.cmd('DiffviewOpen ' .. merge_base)
+        end,
+        desc = 'Diffview vs tracking branch',
+      },
+    },
+  },
 }
