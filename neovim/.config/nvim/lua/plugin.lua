91e46b448969090337114bd58323fdecd4a9590e
wungjae <wungjae@uber.com>
2026-07-24 22:05:51 +0000

    Fix treesitter parser lookup on uber-neovim by re-adding /usr/lib/nvim to rtp

--- neovim/.config/nvim/lua/plugin.lua
+++ neovim/.config/nvim/lua/plugin.lua
@@ -4,4 +4,12 @@ if not vim.uv.fs_stat(lazypath) then
     'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup('plugins')  -- load every file in lua/plugins/
+require('lazy').setup('plugins', {  -- load every file in lua/plugins/
+  performance = {
+    rtp = {
+      -- uber-neovim ships compiled treesitter parsers (markdown, lua, ...) here
+      -- but doesn't add it to runtimepath itself, so lazy's rtp reset must re-add it
+      paths = { '/usr/lib/nvim' },
+    },
+  },
+})
