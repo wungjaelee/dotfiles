828706f38865228d49d7f7786759af97694c3446
Wungjae Lee <wungjae96@gmail.com>
2026-07-25 01:11:14 -0400

    Add terminal Esc keybinding and enable hidden files in file picker

--- neovim/.config/nvim/lua/keys.lua
+++ neovim/.config/nvim/lua/keys.lua
@@ -4,6 +4,9 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to below pane' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to above pane' })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right pane' })
 
+-- exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- save by pressing Escape
 vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
 -- pasting over a selection no longer clobbers your clipboard
--- neovim/.config/nvim/lua/plugins/navigation.lua
+++ neovim/.config/nvim/lua/plugins/navigation.lua
@@ -9,7 +9,7 @@ return {
       input = { enabled = true },
     },
     keys = {
-      { '<leader>f', function() Snacks.picker.files() end, desc = 'Find Files' },
+      { '<leader>f', function() Snacks.picker.files({ hidden = true }) end, desc = 'Find Files' },
       { '<leader>s', function() Snacks.picker.grep() end,  desc = 'Search Text' },
       { '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
       { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
