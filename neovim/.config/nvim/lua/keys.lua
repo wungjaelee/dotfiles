db97365a35dabc25ea3e22a0066e37052234fce9
wungjae <wungjae@uber.com>
2026-07-27 01:30:58 +0000

    Replace selection in vim with <leader>r

--- neovim/.config/nvim/lua/keys.lua
+++ neovim/.config/nvim/lua/keys.lua
@@ -20,3 +20,7 @@ vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle comment', remap = true }
 vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
 -- pasting over a selection no longer clobbers your clipboard
 vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]])
+
+-- replace word under cursor, or visual selection, everywhere in the file
+vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>//g<Left><Left>]], { desc = 'Replace word under cursor' })
+vim.keymap.set('v', '<leader>r', [["hy:%s/\V<C-r>=escape(@h, '/\')<CR>//g<Left><Left>]], { desc = 'Replace selection' })
