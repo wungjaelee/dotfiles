2d598fd35aa1dd5461c954e1dfb84cdc3b5fc68e
wungjae <wungjae@uber.com>
2026-08-17 04:45:18 +0000

    Add noice.nvim for cmdline, popupmenu, and messages UI
    Notifications stay on Snacks' notifier to avoid duplicate UIs.

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -4,4 +4,23 @@ return {
     lazy = false,
     config = true,  -- popup that shows what my leader keys do
   },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        long_message_to_split = true,
+      },
+      -- Snacks already owns notifications; let noice handle cmdline/messages/popupmenu only
+      notify = { enabled = false },
+    },
+  },
 }
